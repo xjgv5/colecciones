f731960bc26ea7260102a321054196ba07e7321a
Javier Gomez <javier.gov@outlook.es>
2023-11-15 22:19:53 -0600

    Eliminar duplicados

---
+++ src/com/xjgv/ejemplos/set/EjemploHashsetBuscarDuplicado2.java
@@ -0,0 +1,21 @@
+package com.xjgv.ejemplos.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class EjemploHashsetBuscarDuplicado {
+    public static void main(String[] args) {
+        String[] peces = {"Corvina", "Lenguado", "Pejerrey", "Robalo",
+        "Atún", "Lenguado"};
+
+        Set<String> unicos = new HashSet<>();
+        for(String pez: peces){
+
+            if (unicos.add(pez) == false){
+                System.out.println("El elemento : " + pez + " está duplicado");
+            }
+        }
+
+        System.out.println(unicos);
+    }
+}
