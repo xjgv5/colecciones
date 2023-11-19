9418f9e79ef646b3a2d849e89b07fc5653cfc0ba
Javier Gomez <javier.gov@outlook.es>
2023-11-18 23:18:45 -0600

    List iterator

--- src/com/xjgv/ejemplos/set/EjemploLinkedList.java
+++ src/com/xjgv/ejemplos/set/EjemploLinkedList.java
@@ -5,6 +5,7 @@ import com.xjgv.ejemplos.modelo.Alumno;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class EjemploLinkedList {
     public static void main(String[] args) {
@@ -31,5 +32,18 @@ public class EjemploLinkedList {
         Alumno alm2 = enlazada.peekLast();
         enlazada.pollFirst();
         enlazada.forEach(System.out::println);
+        enlazada.set(1, new Alumno("Carlos", 2));
+
+        System.out.println("------------------------------");
+        ListIterator<Alumno> li = enlazada.listIterator();
+
+        while (li.hasNext()){
+            System.out.println(li.next());
+        }
+
+        System.out.println("-------------------------------");
+        while (li.hasPrevious()){
+            System.out.println(li.previous());
+        }
     }
 }
