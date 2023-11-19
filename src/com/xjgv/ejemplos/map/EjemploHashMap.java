24d057b4e884a026535b6b6a04c33e9a72e3207d
Javier Gomez <javier.gov@outlook.es>
2023-11-19 00:18:38 -0600

    Metodos de HashMap

--- src/com/xjgv/ejemplos/map/EjemploHashMap.java
+++ src/com/xjgv/ejemplos/map/EjemploHashMap.java
@@ -1,7 +1,6 @@
 package com.xjgv.ejemplos.map;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class EjemploHashMap {
     public static void main(String[] args) {
@@ -18,8 +17,45 @@ public class EjemploHashMap {
         System.out.println("nombre : " + nombre);
         System.out.println("apellido : " + apellido);
 
+        String valorApellido = persona.remove("apellido");
+        System.out.println("eliminado : " + valorApellido);
+        System.out.println(persona);
 
+        boolean b = persona.containsKey("apellido");
+        System.out.println("b = " + b);
 
-        
+        b = persona.containsValue("John");
+        System.out.println("contiene John ? : " + b);
+
+        System.out.println("--------------------------------");
+        Collection<String> valores = persona.values();
+        valores.forEach(System.out::println);
+
+        System.out.println("--------------------------------");
+        Set<String> llaves = persona.keySet();
+        llaves.forEach(System.out::println);
+
+        System.out.println("---------------------------------");
+        for (Map.Entry<String, String> par: persona.entrySet()){
+            System.out.println(par.getKey() + " => " + par.getValue());
+        }
+
+        System.out.println("---------------------------------");
+
+        for (String llave : persona.keySet()){
+            String valor = persona.get(llave);
+            System.out.println(llave + " => " + valor);
+        }
+
+        System.out.println("---------------------------------");
+        persona.forEach((llave, valor) -> {
+            System.out.println(llave + " => " + valor);
+        });
+
+        System.out.println("-----------------------------------");
+        System.out.println("total : " + persona.size());
+        System.out.println("Contiene elementos : " + !persona.isEmpty());
+   
+
     }
 }
